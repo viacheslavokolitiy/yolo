2ff0b18b893ec2f6a1cb7a2ab3ef230332dbbd6e
viacheslavokolitiy <4815162342aAA>
2016-03-25 15:24:57 +0200

    Try to fix codecov

--- ci.sh
+++ ci.sh
@@ -3,4 +3,5 @@
 # Please run it from root project directory
 
 # This will: compile the project, run lint, run tests under JVM, package apk, check the code quality and run tests on the device/emulator.
+./gradlew build jacocoTestReport assembleAndroidTest
 ./gradlew clean build connectedAndroidTest -PdisablePreDex
