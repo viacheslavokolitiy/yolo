f9e1f0a90924deb8a3cb2ef0667a1facc8c71e90
viacheslavokolitiy <4815162342aAA>
2016-04-08 11:48:40 +0300

    Add flag

--- ci.sh
+++ ci.sh
@@ -3,4 +3,4 @@
 # Please run it from root project directory
 
 # This will: compile the project, run lint, run tests under JVM, package apk, check the code quality and run tests on the device/emulator.
-./gradlew clean build connectedAndroidTest -PdisablePreDex --continue --stacktrace --info
+./gradlew clean build connectedAndroidTest -PdisablePreDex --continue --stacktrace --info --debug
